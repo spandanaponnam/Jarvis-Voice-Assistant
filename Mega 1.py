21e54c4d9b1290403b7268717f1d43921336cd39
spandanaponnam <spandanaponnam246@gmail.com>
2026-03-13 22:02:59 +0530

    Initial commit of Jarvis Voice Assistant

--- Mega 1.py
+++ Mega 1.py
@@ -1,39 +1,50 @@
 import webbrowser
 import speech_recognition as sr
 import pyttsx3
-from openai import OpenAI
 recognizer=sr.Recognizer()
 engine = pyttsx3.init()
+# For female Voice
+#voices = engine.getProperty('voices')
+#engine.setProperty('voice', voices[1].id)
 def speak(text):
     engine.say(text)
     engine.runAndWait()
-def openwebsites(c):
+def open_websites(c):
     if "open google" in c.lower():
+        speak("Opening Google")
         webbrowser.open_new("https://google.com")
     elif "open facebook" in c.lower():
+        speak("Opening Facebook")
         webbrowser.open_new("https://facebook.com")
     elif "open youtube" in c.lower():
+        speak("Opening Youtube")
         webbrowser.open_new("https://youtube.com")
     elif "open instagram" in c.lower():
+        speak("Opening Instagram")
         webbrowser.open_new("https://instagram.com")
     elif "open linkedin" in c.lower():
+        speak("Opening Linkedin")
         webbrowser.open_new("https://linkedin.com")
-if __name__=="__main__":
-    speak("Hii i'm Jarvis,i'm initialising....")
+    elif "exit" in c or "stop" in c:
+        speak("Goodbye")
+        exit()
+def main():
+    speak("Hii I'm Jarvis.Initialising....")
     while True:
-        r=sr.Recognizer()
-        print("Recognizing...")
         try:
             with sr.Microphone() as source:
+                print("Adjusting for background noise...")
+                recognizer.adjust_for_ambient_noise(source, duration=1)
                 print("Listening....")
-                audio=r.listen(source, timeout=3, phrase_time_limit=5)
-            command=r.recognize_google(audio)
-            if command.lower()=="jarvis":
-                speak("Yeh hii how can i help u")
-                with sr.Microphone() as source:
-                    print("Listening....")
-                    audio = r.listen(source)
-                command = r.recognize_google(audio)
-                openwebsites(command)
+                audio=recognizer.listen(source)
+            command=recognizer.recognize_google(audio)
+            print("You said:",command)
+            open_websites(command)
+        except sr.UnknownValueError:
+            speak("Could not understand audio repeat it again")
+        except sr.RequestError:
+            speak("There is a network error wait for a while")
         except Exception as e:
-            print("Error;{0}".format(e))
+            print("Error",(e))
+if __name__=="__main__":
+    main()
